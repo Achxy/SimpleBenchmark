8db1e01e4224a72b467e050d941ba8b0a5ac3a7a
Achxy <74144790+Achxy@users.noreply.github.com>
2022-07-25 20:50:00 +0530

    type annotations and sentinels in `typeshack`

--- benchmark/typeshack.py
+++ benchmark/typeshack.py
@@ -1,5 +1,20 @@
-from typing import TypeVar, ParamSpec
-
+from typing import TypeVar, ParamSpec, Literal
+from enum import Enum
 
 R = TypeVar("R")
 P = ParamSpec("P")
+
+
+class _Sentinel(Enum):
+    """
+    Single member enum for static typecheckers to validate
+    code, this is necessitated due to the fact that
+    assigning sentinels to value returned by object()
+    causes incorrect type inference.
+    See, https://peps.python.org/pep-0661/
+    """
+
+    MISSING = object()
+
+
+MISSING: Literal[_Sentinel.MISSING] = _Sentinel.MISSING
