4da62a0b119d5618efa5ad85c9df687a82bc5da6
Achxy <74144790+Achxy@users.noreply.github.com>
2022-07-25 21:42:18 +0530

    added error handling

--- benchmark/builder.py
+++ benchmark/builder.py
@@ -1,10 +1,10 @@
 from typing import Literal
 from .abstract import BaseBenchmark
 from collections.abc import Callable
-from .typeshack import P, R, MISSING
+from .typeshack import P, R, MISSING, Q
 from time import perf_counter, process_time
 from .helpers import get_name
-from .errors import TimeNotMeasuredError
+from .errors import FragmentaryBenchmarkError
 
 
 class SyncBenchmark(BaseBenchmark[P, R]):
@@ -25,20 +25,32 @@ class SyncBenchmark(BaseBenchmark[P, R]):
     def post_benchmark_hook(self) -> None:
         self.show_performance()
 
+    def get_result(self, sentinel: Q = MISSING) -> R | Q:
+        if self._result is MISSING:
+            if sentinel is MISSING:
+                msg = "Benchmark objects have not been invoked and a sentinel argument has not been provided"
+                raise RuntimeError(msg) from FragmentaryBenchmarkError()
+            return sentinel
+        return self._result
+
     @property
     def function(self) -> Callable[P, R]:
         return self._func
 
     @property
+    def result(self) -> R:
+        if self._result is MISSING:
+            raise FragmentaryBenchmarkError()
+        return self._result
+
+    @property
     def process_time_delta(self) -> float:
         if self._process_delta is None:
-            name = get_name(self.function, repr(self))
-            raise TimeNotMeasuredError(name)
+            raise FragmentaryBenchmarkError()
         return self._process_delta
 
     @property
     def perf_time_delta(self) -> float:
         if self._perf_delta is None:
-            name = get_name(self.function, repr(self))
-            raise TimeNotMeasuredError(name)
+            raise FragmentaryBenchmarkError()
         return self._perf_delta
